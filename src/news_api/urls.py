e459128947fad6ed00d24649c48d903e32227df5
Hamedtbf <hamedtabafar@gmail.com>
2024-08-20 20:38:08 +0330

    adding new views to list of urls

--- src/news_api/urls.py
+++ src/news_api/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
-from .views import PostList, PostDetail
+from django.urls import path, re_path
+from .views import PostList, PostDetail, TagList, TagRelatedPosts
 
 urlpatterns = [
     path('list/', PostList.as_view()),
-    path('<int:item>/', PostDetail.as_view())
+    path('<int:post_id>/', PostDetail.as_view()),
+    path('list/tags/', TagList.as_view()),
+    path('list/tag=<str:tag>/', TagRelatedPosts.as_view())
 ]

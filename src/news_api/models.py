766a7dfe8d631b915bac66ad83edd0a3f9ae43ce
Hamedtbf <hamedtabafar@gmail.com>
2024-08-20 19:43:47 +0330

    adding post_id to Post model.

--- src/news_api/models.py
+++ src/news_api/models.py
@@ -5,14 +5,18 @@ from django.db import models
 
 
 class Tag(models.Model):
-    in_persian = models.CharField(max_length=20)
-    in_english = models.CharField(max_length=20)
+    in_english = models.CharField(max_length=20, unique=True)
+    in_persian = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return f'{self.in_english}-{self.in_persian}'
 
 
 class Post(models.Model):
-    item = models.IntegerField(primary_key=True)
+    post_id = models.IntegerField(unique=True)
     title = models.CharField(max_length=50)
     content = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name='posts_of')
 
     def __str__(self):
         return self.title

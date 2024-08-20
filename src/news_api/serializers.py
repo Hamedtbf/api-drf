0bb110161db7d2d893ac4e0e88f227f70d047b95
Hamedtbf <hamedtabafar@gmail.com>
2024-08-20 15:49:47 +0330

    migrating to ModelSerializer

--- src/news_api/serializers.py
+++ src/news_api/serializers.py
@@ -2,16 +2,7 @@ from rest_framework import serializers
 from .models import Post
 
 
-class PostSerializer(serializers.Serializer):
-    item = serializers.IntegerField(read_only=True)
-    title = serializers.CharField()
-    content = serializers.CharField()
-
-    def create(self, validated_data):
-        return Post.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.content = validated_data.get('content', instance.content)
-        instance.save()
-        return instance
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'

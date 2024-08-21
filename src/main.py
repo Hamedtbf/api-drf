ae0f9d9f1ee7b686eaf43ce683ac8f698d238626
Hamedtbf <hamedtabafar@gmail.com>
2024-08-21 07:52:29 +0330

    all done :))

--- src/main.py
+++ src/main.py
@@ -7,26 +7,26 @@ django.setup()
 from gathering_news.extraction import extract_urls, extract_post_data
 from news_api.models import Post, Tag
 from django.db import IntegrityError
+from django.core.exceptions import ObjectDoesNotExist
 
 if __name__ == '__main__':
-    limit = 50
+    limit = 80
     urls = []
     for page_number in range(1, 5):
         urls.extend(extract_urls(page_number))
 
-    count = 0
+    urls = urls[:limit]
     for url in urls:
-        if count < limit:
-            post_id, title, content, tags = extract_post_data(url)
+        post_id, title, content, tags = extract_post_data(url)
+        try:
+            post = Post.objects.create(post_id=post_id, title=title, content=content)
+        except IntegrityError as er:
+            print(f'duplicate post with {post_id} id. killing the process')
+            continue
+        for tag in tags:
             try:
-                post = Post.objects.create(post_id=post_id, title=title, content=content)
-            except IntegrityError as er:
-                print(f'duplicate post with {post_id} id. killing the process')
-                break
-            for tag in tags:
                 existing = Tag.objects.get(in_english=tag[0])
-                if existing:
-                    post.tags.add(existing)
-                else:
-                    tag = Tag.objects.create(in_english=tag[0], in_persian=tag[1])
-                    post.tags.add(tag)
+            except Tag.DoesNotExist as tag_er:
+                existing = Tag.objects.create(in_english=tag[0], in_persian=tag[1])
+            finally:
+                post.tags.add(existing)

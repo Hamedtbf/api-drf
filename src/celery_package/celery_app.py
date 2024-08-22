9f82e1c5dbd6d064f36c545c9bf5724123861475
Hamedtbf <hamedtabafar@gmail.com>
2024-08-22 23:17:03 +0330

    adding scrape_news task to celery_app.py + defining a scheduler

--- src/celery_package/celery_app.py
+++ src/celery_package/celery_app.py
@@ -3,13 +3,18 @@ from gathering_news.main import update_database
 
 app = Celery('extraction')
 
-# Load configuration from a separate file or directly here
-app.config_from_object('celery_package.celery_config')
 
-
-@app.task
+@app.task()
 def scrape_news():
     update_database()
 
 
-app.tasks.register(scrape_news)
+# Load configuration from a separate file or directly here
+app.config_from_object('celery_package.celery_config')
+
+app.conf.beat_schedule = {
+    'add-every-12-hours': {
+        'task': 'celery_package.celery_app.scrape_news',
+        'schedule': 12 * 60 * 60,
+    },
+}

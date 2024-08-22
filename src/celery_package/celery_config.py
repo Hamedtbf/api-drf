cb8c7cf709f49941f5aec9ff0f2e90ebe87f3bc8
Hamedtbf <hamedtabafar@gmail.com>
2024-08-22 19:43:42 +0330

    making configuration for celery app

---
+++ src/celery_package/celery_config.py
@@ -0,0 +1,7 @@
+broker_url = 'redis://localhost:6379/0'
+result_backend = 'redis://localhost:6379/1'
+accept_content = ['json']
+task_serializer = 'json'
+result_serializer = 'json'
+timezone = 'Asia/Tehran'
+enable_utc = False

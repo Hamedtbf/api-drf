b0d185e087a4a51b1b350f5ebc89848b02c6bb9e
Hamedtbf <hamedtabafar@gmail.com>
2024-08-22 21:28:05 +0330

    adding some logs to update_database function to improve readability

--- src/gathering_news/main.py
+++ src/gathering_news/main.py
@@ -1,5 +1,6 @@
 import os
 import django
+import logging
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zoomit.settings')
 django.setup()
@@ -11,18 +12,21 @@ from django.core.exceptions import ObjectDoesNotExist
 
 
 def update_database():
-    limit = 80
     urls = []
     for page_number in range(1, 5):
         urls.extend(extract_urls(page_number))
 
-    urls = urls[:limit]
+    logging.basicConfig(level=logging.INFO)
+
+    before = len(Post.objects.all())
+    dup, new = 0, 0
     for url in urls:
         post_id, title, content, tags = extract_post_data(url)
         try:
             post = Post.objects.create(post_id=post_id, title=title, content=content)
         except IntegrityError as er:
-            print(f'duplicate post with {post_id} id. killing the process')
+            logging.info(f'duplicate post with {post_id} id. going to the next post')
+            dup = dup + 1
             continue
         for tag in tags:
             try:
@@ -31,6 +35,16 @@ def update_database():
                 existing = Tag.objects.create(in_english=tag[0], in_persian=tag[1])
             finally:
                 post.tags.add(existing)
+
+        logging.info(f'uploading a new post with {post_id} id')
+        new = new + 1
+
+    logging.info(f'posts count before updating API: {before}')
+    logging.info(f'new posts count: {new}')
+    logging.info(f'duplicate posts count: {dup}')
+    logging.info(f'posts count after updating API: {before + new}')
+
+
 
 
 if __name__ == '__main__':

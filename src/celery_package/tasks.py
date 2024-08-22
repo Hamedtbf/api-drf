5627a7fbb28082730b088ab75a23b013f638c066
Hamedtbf <hamedtabafar@gmail.com>
2024-08-22 21:09:23 +0330

    removing tasks.py

--- src/celery_package/tasks.py
+++ src/celery_package/tasks.py
@@ -1,8 +1,1 @@
-from celery_package.celery_app import app
-from gathering_news.main import update_database  # Import your function
-
-
-@app.task
-def scrape_news():
-    # Call the original function inside the Celery task
-    update_database()
+from celery_app import app
